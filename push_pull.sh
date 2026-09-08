4b5ef2145a003ab7e02d5ef8727e111f546354b2
Aleksandr Ostapchuk <87189132+ocaflash@users.noreply.github.com>
2026-09-08 12:05:06 +0300

    Update push_pull.sh

--- push_pull.sh
+++ push_pull.sh
@@ -1,7 +1,7 @@
 ln -sf /opt/node_modules/push-server/etc/push-server /etc/push-server
 cd /opt/node_modules/push-server
-cp etc/init.d/push-server-multi /usr/local/bin/push-server-multi
+cp /etc/init.d/push-server-multi /usr/local/bin/push-server-multi
 mkdir /etc/sysconfig
-cp etc/sysconfig/push-server-multi  /etc/sysconfig/push-server-multi
-cp etc/push-server/push-server.service  /etc/systemd/system/
+cp /etc/sysconfig/push-server-multi  /etc/sysconfig/push-server-multi
+cp /etc/push-server/push-server.service  /etc/systemd/system/
 ln -sf /opt/node_modules/push-server /opt/push-server
