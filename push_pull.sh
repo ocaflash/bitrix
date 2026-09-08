9eca0c05a70f79426c2e2f2e2e6cd84fa9637cfd
Aleksandr Ostapchuk <87189132+ocaflash@users.noreply.github.com>
2026-09-08 12:25:52 +0300

    Update push_pull.sh

--- push_pull.sh
+++ push_pull.sh
@@ -1,6 +1,17 @@
 cat /dev/urandom |tr -dc A-Za-z0-9 | head -c 128
 
-GROUP=web1-client0
+В /etc/sysconfig/push-server-multi ставим:
+
+USER=web1
+GROUP=client0
 SECURITY_KEY="kv2UTopYxfLSW9PMOFjyOamQQD5WU7NMPlsM7FelBf9QBIxBbYztNFapClXpf3Gp9i7XptX72rU2nFyNcBhUtQCgODqmzmQmfdXXPcHA98lkOUY3NyqCYNKocXmw5EAw"
 RUN_DIR=/tmp/push-server
 REDIS_SOCK=/run/redis/redis.sock
+
+А в /etc/systemd/system/push-server.service:
+
+[Service]
+User=web1
+Group=client0
+ExecStart=/usr/local/bin/push-server-multi systemd_start
+ExecStop=/usr/local/bin/push-server-multi stop
