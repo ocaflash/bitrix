397650d22764d0b6d60fac7f7f22b5250f7bdf45
Aleksandr Ostapchuk <87189132+ocaflash@users.noreply.github.com>
2026-09-08 14:24:34 +0300

    Update push_pull.sh

--- push_pull.sh
+++ push_pull.sh
@@ -9,5 +9,3 @@ systemd-tmpfiles --remove --create
 
 systemctl daemon-reload
 systemctl --now enable push-server
-
-kv2UTopYxfLSW9PMOFjyOamQQD5WU7NMPlsM7FelBf9QBIxBbYztNFapClXpf3Gp9i7XptX72rU2nFyNcBhUtQCgODqmzmQmfdXXPcHA98lkOUY3NyqCYNKocXmw5EAw
